349002f785e646997e53f7cb2beff147db82445d
amitswaraj61 <aswaraj78@gmail.com>
2020-10-02 15:55:42 +0530

    Added valid login Test

--- BookswagonAzures/BookswagonTest/Test.cs
+++ BookswagonAzures/BookswagonTest/Test.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BookswagonAzures.BookswagonTest
 {
-   public class Test : Base
+    public class Test : Base
     {
         [Obsolete]
         [Test, Order(1)]
@@ -21,62 +21,5 @@ namespace BookswagonAzures.BookswagonTest
             string expectedResult = "https://www.bookswagon.com/myaccount.aspx";
             Assert.AreEqual(expectedResult, actualResult);
         }
-
-        [Test, Order(2)]
-        [System.Obsolete]
-        public void SearchBookTest()
-        {
-            Search search = new Search(driver);
-            search.SearchBook();
-            string url = "https://www.bookswagon.com/search-books/mahabharata";
-            string actual = driver.Url;
-            Assert.AreEqual(url, actual);
-        }
-
-        [Test, Order(3)]
-        [System.Obsolete]
-        public void BuyBookTest()
-        {
-            SearchHomePage search = new SearchHomePage(driver);
-            search.PlaceOrderBook();
-            string url = "https://www.bookswagon.com/checkout-login";
-            string actual = driver.Url;
-            Assert.AreEqual(url, actual);
-        }
-
-        [Test, Order(4)]
-        [System.Obsolete]
-        public void ShippingAddressTest()
-        {
-            ShippingAddress address = new ShippingAddress(driver);
-            address.ShippingAddessData();
-            string url = "https://www.bookswagon.com/viewshoppingcart.aspx";
-            string actual = driver.Url;
-            Assert.AreEqual(url, actual);
-        }
-
-        [Test, Order(5)]
-        [System.Obsolete]
-        public void ReviewOderTest()
-        {
-            ReviewOrder review = new ReviewOrder(driver);
-            review.ReviewOrderCheck();
-            string url = "https://www.bookswagon.com/checkout.aspx";
-            string actual = driver.Url;
-            Assert.AreEqual(url, actual);
-        }
-
-        [Test, Order(6)]
-        [System.Obsolete]
-        public void LogoutTest()
-        {
-            Logout logout = new Logout(driver);
-            logout.LogoutToBookSwagaon();
-            string url = "https://www.bookswagon.com/login";
-            string actual = driver.Url;
-            Assert.AreEqual(url, actual);
-        }
     }
 }
-
- 
